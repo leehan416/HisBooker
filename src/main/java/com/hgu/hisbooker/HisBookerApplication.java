78d04aa71126a0a05d64c1541e85490bf6d43a92
Whal3 <leehan416@gmail.com>
2025-02-22 19:50:50 +0900

    project init

---
+++ src/main/java/com/hgu/hisbooker/HisBookerApplication.java
@@ -0,0 +1,13 @@
+package com.hgu.hisbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisBookerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HisBookerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hgu/hisbooker/HisBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgu.hisbooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisBookerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
